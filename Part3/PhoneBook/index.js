d9ab34a309931bea98cec198a816b0730ad65c0c
Monica Mourad <monicamourad18@hotmail.com>
2025-02-24 18:04:01 +0100

    Submit Exercise 3.7

--- Part3/PhoneBook/index.js
+++ Part3/PhoneBook/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const morgan = require('morgan')
+
 const port = 3001;
 const app = express();
 app.use(express.json());
+app.use(morgan('tiny'));
 
 let notes = [
   {
