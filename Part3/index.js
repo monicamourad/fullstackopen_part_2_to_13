f40f780202c90d8adc77a1ef897b538bfe7be8ce
Monica Mourad <monicamourad18@hotmail.com>
2025-02-24 13:34:17 +0100

    Submit Exercise 3.2

--- Part3/index.js
+++ Part3/index.js
@@ -1,36 +1,42 @@
-const express = require('express');
+const express = require("express");
 const port = 3001;
 const app = express();
 
 const notes = [
-    [
-        { 
-          "id": "1",
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": "2",
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": "3",
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": "4",
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    ]
+  [
+    {
+      id: "1",
+      name: "Arto Hellas",
+      number: "040-123456",
+    },
+    {
+      id: "2",
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+    },
+    {
+      id: "3",
+      name: "Dan Abramov",
+      number: "12-43-234345",
+    },
+    {
+      id: "4",
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+    },
+  ],
 ];
 
-app.get('/api/persons', (req, res) => {
+app.get("/api/persons", (req, res) => {
   res.send(notes);
-})
+});
+
+app.get("/api/info", (req, res) => {
+  const message = `PhoneBook has info for ${notes[0].length} people \n${Date()}`;
+  res.set("Content-Type", "text/plain");
+  res.send(message);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
